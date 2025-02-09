1a29fab1e565406407e5800e8c558f926da2c1ae
Andreas Åkerberg <zmallwood@proton.me>
2025-02-09 12:57:00 +0100

    Save

---
+++ src/Theme0.Core.Common/MathLibrary.hpp
@@ -0,0 +1,43 @@
+// Copyright 2025 Andreas Åkerberg
+
+#pragma once
+
+namespace ForradiaWorld
+{
+    class Point
+    {
+      public:
+        int x { 0 };
+        int y { 0 };
+    };
+
+    class PointF
+    {
+      public:
+        float x { 0.0f };
+        float y { 0.0f };
+    };
+
+    class RectF
+    {
+      public:
+        float x { 0.0f };
+        float y { 0.0f };
+        float w { 0.0f };
+        float h { 0.0f };
+    };
+
+    class Size
+    {
+      public:
+        int w { 0 };
+        int h { 0 };
+    };
+
+    class SizeF
+    {
+      public:
+        float w { 0.0f };
+        float h { 0.0f };
+    };
+}
